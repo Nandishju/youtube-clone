a12a29ff500eb6d3c0cfdf44f92925be9e7ff2b4
J U <nandish.j-u@capgemini.com>
2022-09-19 23:41:49 +0530

    added react player,responsive, and deployed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     return (
-        <Card sx={{ width: {  md: "320px", xs:"100%" }, boxShadow: "none", borderRadius: 0 }}>
+        <Card sx={{ width: { xs:'100%',  md: "320px"}, boxShadow: "none", borderRadius: 0 }}>
         <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
           <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
             sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
